f39812298228d8cf5fcb1410ffbb063ef8505a5a
VGalushkina <vgalushkina@bekitzur.com>
2020-03-12 15:32:16 +0300

    test4Caastle

--- src/doingThings.java
+++ src/doingThings.java
@@ -1,13 +1,10 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
-//import org.sellcom.javafx.*;
+
 
 public class doingThings {
 	public String baseUrl = "https://camelot-qa.caastle.com/password";
@@ -52,8 +49,10 @@ public class doingThings {
 		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#Fill-1")));
 		WebElement noneSubscriber = driver.findElement(By.cssSelector("#subscribe_link_nh"));
 		Assert.assertFalse(noneSubscriber.isDisplayed());
-		WebElement subscribersName = driver.findElement(By.cssSelector("body.ae-lang-en.ae-device-desktop.ae-:nth-child(2) div.page-wrapper.below.header:nth-child(48) div.header-v1-container.header-elements-container:nth-child(2) header.row.expanded.clearfix.header-common.header-elements:nth-child(1) section.logo-nav-container section.logo-bar.clearfix.promotion-bar-new section.small-4.medium-4.column.right-side-links.new-utility-header:nth-child(12) div.contact-info.utility-header:nth-child(2) div.head-nav div.customer.clearfix ul.row.closet-link-ul li.p-relative.user-dd-wrap.gb-point:nth-child(4) > div.customer-name-header.hide-for-small-only.new-header-dropdown-link.customer-name-link"));
-		String specialClass = subscribersName.getAttribute("class");
+
+		WebElement Subscriber = driver.findElement(By.cssSelector("body.ae-lang-en.ae-device-desktop.ae-:nth-child(2) div.page-wrapper.below.header:nth-child(48) div.header-v1-container.header-elements-container:nth-child(2) header.row.expanded.clearfix.header-common.header-elements:nth-child(1) section.logo-nav-container section.logo-bar.clearfix.promotion-bar-new section.small-4.medium-4.column.right-side-links.new-utility-header:nth-child(12) div.contact-info.utility-header:nth-child(2) div.head-nav div.customer.clearfix ul.row.closet-link-ul > li.p-relative.user-dd-wrap.gb-point:nth-child(4)"));
+		String specialClass = Subscriber.getAttribute("class");
+		System.out.println(specialClass);
 		Assert.assertTrue(specialClass.contains("customer-name-header"));
 	}
 
@@ -63,14 +62,16 @@ public class doingThings {
 		WebElement openAtqCollection = driver.findElement(By.cssSelector("#atqa-collection"));
 		openAtqCollection.click();
 
+
 		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#BreadFiltComb")));
 		WebElement atqaCollection = driver.findElement(By.cssSelector("body.ae-lang-en.ae-device-desktop.ae-:nth-child(2) div.page-wrapper.below.header.collection-scroll-fix:nth-child(48) div.page-wrapper-below-header:nth-child(4) div.main-content.main-content-new-header:nth-child(1) section.clearfix div.responsive.seo-collection-wrapper.faceted-search-hb-div.clearfix.collection-variation-b:nth-child(2) div.clearfix.faceted-mobile:nth-child(4) section.column.small-12.medium-9.gb-product-section:nth-child(2) ul.filter-combo.bread-filt-comb:nth-child(1) li:nth-child(1) > h2.subtitle-seo"));
 		String atqaCollectionText = atqaCollection.getText();
 		System.out.println(atqaCollectionText);
 		Assert.assertTrue(atqaCollectionText.equalsIgnoreCase("ATQA collection"));
+
 	}
 
-	@Test (priority = 5)
+	@Test(priority = 5)
 	public void verifyProductPage() {
 		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body.ae-lang-en.ae-device-desktop.ae-:nth-child(2) div.page-wrapper.below.header.collection-scroll-fix:nth-child(48) div.page-wrapper-below-header:nth-child(4) div.main-content.main-content-new-header:nth-child(1) section.clearfix div.responsive.seo-collection-wrapper.faceted-search-hb-div.clearfix.collection-variation-b:nth-child(2) div.clearfix.faceted-mobile:nth-child(4) section.column.small-12.medium-9.gb-product-section:nth-child(2) ul.row.small-up-2.medium-up-3.large-up-3.small-block-grid-2.medium-block-grid-3.large-block-grid-3.product-ul.text-center:nth-child(2) li.column.column-block.pos-rel:nth-child(1) div.hover-item a.js-product-anchor div:nth-child(1) > img.full-width.product-img.ae-img")));
 		driver.findElement(By.cssSelector("body.ae-lang-en.ae-device-desktop.ae-:nth-child(2) div.page-wrapper.below.header.collection-scroll-fix:nth-child(48) div.page-wrapper-below-header:nth-child(4) div.main-content.main-content-new-header:nth-child(1) section.clearfix div.responsive.seo-collection-wrapper.faceted-search-hb-div.clearfix.collection-variation-b:nth-child(2) div.clearfix.faceted-mobile:nth-child(4) section.column.small-12.medium-9.gb-product-section:nth-child(2) ul.row.small-up-2.medium-up-3.large-up-3.small-block-grid-2.medium-block-grid-3.large-block-grid-3.product-ul.text-center:nth-child(2) li.column.column-block.pos-rel:nth-child(1) div.hover-item a.js-product-anchor div:nth-child(1) > img.full-width.product-img.ae-img")).click();
@@ -81,26 +82,120 @@ public class doingThings {
 		Assert.assertTrue(productText.contains("PRODUCT"));
 	}
 
+	@Test(priority = 6)
+	public void verifyItemWasAdded() {
+
+		//pick size and add to cart
+		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body.ae-lang-en.ae-device-desktop.ae-:nth-child(2) div.page-wrapper.below.header:nth-child(49) div.page-wrapper-below-header:nth-child(4) div.main-content.main-content-new-header:nth-child(1) section.clearfix div.row.responsive.m-t.product-page-container div.product-detail-responsive.test-cam-product-71.one_image.clearfix div.product_body.clearfix div:nth-child(1) div.small-12.large-7.medium-7.columns.galen-left-column:nth-child(2) div.product-large-image.featured.small-12.large-10.medium-10.columns.image:nth-child(2) div:nth-child(2) > img.ae-img")));
+		driver.findElement(By.cssSelector("#btn-size-100071-BLK-S")).click();
+		driver.findElement(By.cssSelector("#add-to-cart")).click();
+
+		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.className("size-selector__in-closet-label")));
+		WebElement closetLabel = driver.findElement(By.className("size-selector__in-closet-label"));
+		String closetLabelText = closetLabel.getText();
+		System.out.println(closetLabelText);
+		Assert.assertTrue(closetLabelText.contains("IN CLOSET"));
+/*
+		new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#unclosetable_message")));
+		WebElement changedButton = driver.findElement(By.cssSelector("#unclosetable_message"));
+		String changedButtonText = changedButton.getText();
+		System.out.println(changedButtonText);
+		//Assert.assertTrue(changedButtonText.equalsIgnoreCase("In your closet"));
+ */
+	}
+
+	@Test(priority = 7)
+	public void verifyClosetPage() {
+
+		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#closet_link_nh")));
+		driver.findElement(By.cssSelector("#closet_link_nh")).click();
+
+		new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated((By.cssSelector("#on-rack-li"))));
+		WebElement closetHeadingRackElement = driver.findElement(By.cssSelector("#on-rack-li"));
+		String closetHeadingBarElementText = closetHeadingRackElement.getText();
+		System.out.println(closetHeadingBarElementText);
+		closetHeadingRackElement.click();
+	}
+
+	@Test (priority = 8)
+	public boolean verifyRankPage() {
+		new WebDriverWait(driver,10).until(ExpectedConditions.urlContains("#onRack"));
+		System.out.println("On-the-Rank-Page! Wow!");
+
+		WebElement closetHeadingRackActiveElement = driver.findElement(By.cssSelector("#on-rack-li"));
+
+
+		//(WebElement closetHeadingRackActiveElement) {
+			String classes = closetHeadingRackActiveElement.getAttribute("class");
+				for (String c: classes.split("")) {
+					if (c.equals("top-nav-items closet-heading-bar__menu-item closet-heading-bar__menu-item--on-the-rack active")) {
+						return true;
+					}
+				}
+		return false;
+	}
+
+
+
+
+
+
+
+
+		//System.out.println(closetHeadingRackActiveElement.getClass());
+	}
+
+
+
+/*
+		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#on-rack-li")));
+		WebElement rank = driver.findElement(By.cssSelector("#on-rack-li"));
+		String rankText = rank.getText();
+		System.out.println(rankText);
+		Assert.assertTrue(rankText.equalsIgnoreCase("ON-THE-RACK"));
+*/
+
+
+
+
+/*
+	@Test (priority = 8)
+	public void verifyItemOptions() {
+		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.className("on-rack-products row ppt ppb closet-items-wrapper")));
+		new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#option-100071-BLK-S")));
+		driver.findElement(By.cssSelector("#option-100071-BLK-S")).getText();
+		//WebElement dropMenu = driver.findElement((By.className("bottom-buttons-wrap")));
+		//System.out.println(dropMenu.getText());
+
+
+
+ */
+
+/*
+	@Test (priority = 9)
+	public void verifyDelete(){
+		new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.className("bottom-buttons-wrap")));
+		driver.findElement(By.cssSelector("#100071-BLK-S-delete")).click();
+
+	}
+
+ */
+
+
+/*
+
+
 	@Test (priority = 6)
-	public void verifyEmptyCloset() {
-		WebElement conunter = driver.findElement(By.cssSelector("body.ae-lang-en.ae-device-desktop.ae-:nth-child(2) div.page-wrapper.below.header:nth-child(49) div.header-v1-container.header-elements-container:nth-child(2) header.row.expanded.clearfix.header-common.header-elements:nth-child(1) section.logo-nav-container section.logo-bar.clearfix.promotion-bar-new section.small-4.medium-4.column.right-side-links.new-utility-header:nth-child(12) div.contact-info.utility-header:nth-child(2) div.head-nav div.customer.clearfix ul.row.closet-link-ul li.closet-link-li.p-relative.gb-point:nth-child(1) a:nth-child(1) span.cart-total-items.cart-total-items-nh:nth-child(2) > span.count"));
-		String counterText = conunter.getText();
-		System.out.println(counterText);
-		Assert.assertTrue(counterText.equalsIgnoreCase("0"));
-		//Assert.assertTrue(counterText.contains("0"));
-	}
-
-	@Test (priority = 7)
 	public boolean verifyNoProductsFromThisPageInCloset() {
-		try {
-			driver.findElement(By.className("size-selector__in-closet-label"));
-			return true;
-		}
-		catch (NoSuchElementException e) {
-			return false;
+
+			driver.findElement(By.className("size-selector__in-closet-label")).isDisplayed().then(function(displayed)) {
+				expect(display).not.toBe(true);
+
+
+
 		}
 	}
-
+*/
 
 
 
@@ -109,8 +204,8 @@ public class doingThings {
 	public void endSession () {
 		driver.quit();
 	}
+*/
 
- */
-}
+
 
 
